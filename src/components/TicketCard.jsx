c3d2e9d9a54fa4eae0f30c07853c3d4c7acc65d2
Vishal Jaiswal <vishal.jaiswal@incedoinc.com>
2023-10-12 16:28:40 +0530

    positioning

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -17,7 +17,7 @@ export default function TicketCard({name, ticket}) {
             <div>
                 <ul>
                     <li>
-                        <div className="shadow-lg p-3 ticketcard">
+                        <div className="shadow-lg col-lg-12 p-3 ">
                             <div className="d-flex">
                                 <div className="mx-3 p-3"><h3 className="bg-dark text-white border-rounded p-2">{name.split(" ")[0][0]+name.split(" ")[1][0]}</h3></div>
                                 <div className="p-3"> <h3>{ticket.subject} </h3><span class={`badge text-bg-${statusClass}`}>{ticket.status}</span></div>                              
